0dc7b6001aa3dab75de2fec3f40b2825ec8c62e8
Eric Li <ericli1016@gmail.com>
2015-02-05 16:30:35 -0600

    Elements now expend with window

--- GUI.py
+++ GUI.py
@@ -4,9 +4,15 @@ import threading
 
 class MainFrame():
     def __init__(self, parent):
+
+        parent.columnconfigure(0, weight = 1)
+        parent.rowconfigure(0, weight = 1)
+        
         frame = Frame(parent)
         frame.grid(sticky=W+E+N+S)
-
+        frame.columnconfigure(0, weight = 1)
+        frame.rowconfigure(1, weight = 1)
+        
         self.label = Label(frame, text = "Graph", bg = "red", fg="white")
         self.label.grid(row=0, column=0, columnspan=1, sticky=W+E)
 
