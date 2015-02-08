3cef33bc53395128676731fbd991b43ab0443870
Eric Li <ericli1016@gmail.com>
2015-02-08 12:47:30 -0600

    Created function buildradio. Added value to radiobuttons. Creat radiobuttons in for loop.

--- GUI.py
+++ GUI.py
@@ -8,36 +8,46 @@ class MainFrame():
         parent.columnconfigure(0, weight = 1)
         parent.rowconfigure(0, weight = 1)
         
-        frame = Frame(parent)
-        frame.grid(sticky=W+E+N+S)
-        frame.columnconfigure(0, weight = 1)
-        frame.rowconfigure(1, weight = 1)
+        self.frame = Frame(parent)
+        self.frame.grid(sticky=W+E+N+S)
+        self.frame.columnconfigure(0, weight = 1)
+        self.frame.rowconfigure(1, weight = 1)
         
-        self.label = Label(frame, text = "Graph", bg = "red", fg="white")
+        self.label = Label(self.frame, text = "Graph", bg = "red", fg="white")
         self.label.grid(row=0, column=0, columnspan=1, sticky=W+E)
 
-        self.canvas = Canvas(frame, width = 500, height = 500, bg = 'white')
+        self.canvas = Canvas(self.frame, width = 500, height = 500, bg = 'white')
         self.canvas.grid(row=1, column=0, sticky=W+E+N+S)
 
-        bttnframe = Frame(frame)
-        bttnframe.grid(row=2, column=0, sticky= (W, E))
-        bttnframe.columnconfigure(0, weight = 1)
-        bttnframe.columnconfigure(1, weight = 1)
+        self.bttnframe = Frame(self.frame)
+        self.bttnframe.grid(row=2, column=0, sticky= (W, E))
+        self.bttnframe.columnconfigure(0, weight = 1)
+        self.bttnframe.columnconfigure(1, weight = 1)
 
-        self.bttn1 = Button(bttnframe, text = "Previous")
+        self.bttn1 = Button(self.bttnframe, text = "Previous")
         self.bttn1.grid(row=0, column = 0, sticky=(W, E))
 
-        self.bttn2 = Button(bttnframe, text = "Next")
+        self.bttn2 = Button(self.bttnframe, text = "Next")
         self.bttn2.grid(row=0, column = 1, sticky=(W, E))
+        self.buildradio()
 
-        radiobttnframe = Frame(frame)
+    def buildradio(self):
+
+        MODES = [
+            ("Flatline", 0),
+            ("Square Wave", 1),
+            ("Cosine Wave", 2)]
+
+        v = IntVar()
+        v.set(0)
+
+        radiobttnframe = Frame(self.frame)
         radiobttnframe.grid(row=1, column=1, sticky=(N, S))
-        radiobttn1 = Radiobutton(radiobttnframe, text="Mode 1")
-        radiobttn1.grid(row=0, sticky=W, padx=30)
-        radiobttn2 = Radiobutton(radiobttnframe, text="Mode 2")
-        radiobttn2.grid(row=1, sticky=W, padx=30)
-        radiobttn3 = Radiobutton(radiobttnframe, text="Mode 3")
-        radiobttn3.grid(row=2, sticky=W, padx=30)
+
+        for text, mode in MODES:
+            radiobttn = Radiobutton(radiobttnframe, text=text,
+                                    variable=v, value = mode)
+            radiobttn.grid(row=mode, sticky = W)
 
 root = Tk()
 MainFrame(root)
