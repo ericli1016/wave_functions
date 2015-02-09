2020b84f15de8ce996cb5aa5505d699c289a0885
Eric Li <ericli1016@gmail.com>
2015-02-08 23:27:35 -0600

    Added square_wave function

--- GUI.py
+++ GUI.py
@@ -65,23 +65,39 @@ class MainFrame():
         self.radiovariable.set((mode + 1) % len(self.radiobttnlist))
 
     def draw_axis(self):
-        self.x_axis = self.canvas.create_line(
+        x_axis = self.canvas.create_line(
             0, self.canvas.winfo_height()/2,
             self.canvas.winfo_width(), self.canvas.winfo_height()/2,
             width = 1)
-        self.y_axis = self.canvas.create_line(
+        y_axis = self.canvas.create_line(
             self.canvas.winfo_width()/2-180, 0,
             self.canvas.winfo_width()/2-180, self.canvas.winfo_height(),
             width = 1)
 
     def draw_fn(self):
-        self.flatline()
+        self.square_wave()
 
     def flatline(self):
-        self.wave = self.canvas.create_line(
+        wave = self.canvas.create_line(
             0, self.canvas.winfo_height()/2,
             self.canvas.winfo_width(), self.canvas.winfo_height()/2,
             width = 3)
+        
+    def square_wave(self):
+        x_origin = self.canvas.winfo_width()/2-180
+        y_origin = self.canvas.winfo_height()/2
+        delta = 200
+        x0 = 0
+        y0 = y_origin + delta
+        for x in range(0,self.canvas.winfo_width()):
+            if ((x-x_origin)%360 < 180):
+                y = y_origin - delta
+            else:
+                y = y_origin + delta
+
+            self.canvas.create_line(x0, y0, x, y, width=3)
+            x0 = x
+            y0 = y 
         
     def resize(self, event):
         self.canvas.delete("all")
